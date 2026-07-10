4c1b894f36aa00ec0d9894180b5633c3d57d582c
Chalavadi Dnvh Subbarao <hanumach442@gmail.com>
2026-07-10 22:37:59 +0530

    Create  solution.sql

---
+++ Module 1 - Advanced SQL Server /Views.pdf / Exercise 1 - Create a Simple View /solution.sql
@@ -0,0 +1,20 @@
+USE EmployeeManagementSystem;
+GO
+
+CREATE VIEW vw_EmployeeBasicInfo
+AS
+SELECT
+    e.EmployeeID,
+    e.FirstName,
+    e.LastName,
+    d.DepartmentName
+FROM Employees e
+INNER JOIN Departments d
+    ON e.DepartmentID = d.DepartmentID;
+
+GO
+
+SELECT *
+FROM vw_EmployeeBasicInfo;
+
+GO
