1a23ac6ac77fd3633e31a7ab2bc2baab2affe597
Chalavadi Dnvh Subbarao <hanumach442@gmail.com>
2026-07-10 23:33:36 +0530

    Create  solution.sql

---
+++ Module 1 - Advanced SQL Server /unctions.pdf / Exercise 6 - Execute a User-Defined Function /solution.sql
@@ -0,0 +1,12 @@
+USE EmployeeManagementSystem;
+GO
+
+SELECT
+    EmployeeID,
+    FirstName,
+    LastName,
+    Salary,
+    dbo.fn_CalculateAnnualSalary(Salary) AS AnnualSalary
+FROM Employees;
+
+GO
