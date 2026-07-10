35afbcf99e8da2ef6c61d05469136984c4fb2726
Chalavadi Dnvh Subbarao <hanumach442@gmail.com>
2026-07-10 23:20:24 +0530

    Create  solution.sql

---
+++ Module 1 - Advanced SQL Server /Functions.pdf / Exercise 1 - Create a Scalar Function /solution.sql
@@ -0,0 +1,23 @@
+USE EmployeeManagementSystem;
+GO
+
+CREATE FUNCTION fn_CalculateAnnualSalary
+(
+    @Salary DECIMAL(10,2)
+)
+RETURNS DECIMAL(10,2)
+AS
+BEGIN
+    RETURN @Salary * 12;
+END;
+GO
+
+SELECT
+    EmployeeID,
+    FirstName,
+    LastName,
+    Salary,
+    dbo.fn_CalculateAnnualSalary(Salary) AS AnnualSalary
+FROM Employees;
+
+GO
