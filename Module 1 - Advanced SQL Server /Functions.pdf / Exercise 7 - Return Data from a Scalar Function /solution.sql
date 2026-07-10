168c82cd07935f61a962a582d5de1076d30bd193
Chalavadi Dnvh Subbarao <hanumach442@gmail.com>
2026-07-10 23:35:50 +0530

    Create  solution.sql

---
+++ Module 1 - Advanced SQL Server /Functions.pdf / Exercise 7 - Return Data from a Scalar Function /solution.sql
@@ -0,0 +1,13 @@
+USE EmployeeManagementSystem;
+GO
+
+SELECT
+    EmployeeID,
+    FirstName,
+    LastName,
+    Salary,
+    dbo.fn_CalculateAnnualSalary(Salary) AS AnnualSalary
+FROM Employees
+WHERE EmployeeID = 1;
+
+GO
