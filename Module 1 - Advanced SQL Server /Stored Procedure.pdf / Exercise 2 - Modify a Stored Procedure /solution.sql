4338467b95c98929febbf5192c28c1110cc524ae
Chalavadi Dnvh Subbarao <hanumach442@gmail.com>
2026-07-10 22:48:28 +0530

    Create  solution.sql

---
+++ Module 1 - Advanced SQL Server /Stored Procedure.pdf / Exercise 2 - Modify a Stored Procedure /solution.sql
@@ -0,0 +1,21 @@
+USE EmployeeManagementSystem;
+GO
+
+ALTER PROCEDURE sp_GetEmployeesByDepartment
+    @DepartmentID INT
+AS
+BEGIN
+    SELECT
+        EmployeeID,
+        FirstName,
+        LastName,
+        DepartmentID,
+        Salary,
+        JoinDate
+    FROM Employees
+    WHERE DepartmentID = @DepartmentID;
+END;
+GO
+
+EXEC sp_GetEmployeesByDepartment 1;
+GO
