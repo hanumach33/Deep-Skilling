d6e63899772f2b87eb1ab1bfeb6f801e32d9b78a
Chalavadi Dnvh Subbarao <hanumach442@gmail.com>
2026-07-10 23:38:00 +0530

    Create  solution.sql

---
+++ Module 1 - Advanced SQL Server /Functions.pdf / Exercise 9 - Create a Nested User-Defined Function /solution.sql
@@ -0,0 +1,26 @@
+USE EmployeeManagementSystem;
+GO
+
+CREATE FUNCTION fn_CalculateTotalCompensation
+(
+    @Salary DECIMAL(10,2)
+)
+RETURNS DECIMAL(10,2)
+AS
+BEGIN
+    RETURN dbo.fn_CalculateAnnualSalary(@Salary)
+         + dbo.fn_CalculateBonus(@Salary);
+END;
+GO
+
+SELECT
+    EmployeeID,
+    FirstName,
+    LastName,
+    Salary,
+    dbo.fn_CalculateAnnualSalary(Salary) AS AnnualSalary,
+    dbo.fn_CalculateBonus(Salary) AS Bonus,
+    dbo.fn_CalculateTotalCompensation(Salary) AS TotalCompensation
+FROM Employees;
+
+GO
