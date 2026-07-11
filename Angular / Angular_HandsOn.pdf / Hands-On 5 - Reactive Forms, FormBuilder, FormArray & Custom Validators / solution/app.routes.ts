04f2e98553c7a6e5e233e59de0e3bfaa350bc2c3
Chalavadi Dnvh Subbarao <hanumach442@gmail.com>
2026-07-11 16:58:23 +0530

    Create app.routes.ts

---
+++ Angular / Angular_HandsOn.pdf / Hands-On 5 - Reactive Forms, FormBuilder, FormArray & Custom Validators / solution/app.routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+
+import { ReactiveEnrollmentFormComponent }
+
+from './pages/reactive-enrollment-form/reactive-enrollment-form.component';
+
+export const routes: Routes = [
+
+{
+
+path:'enroll-reactive',
+
+component:ReactiveEnrollmentFormComponent
+
+}
+
+];
