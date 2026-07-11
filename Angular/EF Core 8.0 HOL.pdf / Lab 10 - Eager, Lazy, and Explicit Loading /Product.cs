3f51ad0e6a559fb82c4d70573feeb70c30e0b529
Chalavadi Dnvh Subbarao <hanumach442@gmail.com>
2026-07-11 13:03:42 +0530

    Create  Product.cs

---
+++ Angular/EF Core 8.0 HOL.pdf / Lab 10 - Eager, Lazy, and Explicit Loading /Product.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+
+namespace RetailInventory.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int StockQuantity { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public virtual Category Category { get; set; }
+
+        public virtual List<Tag> Tags { get; set; } = new();
+    }
+}
