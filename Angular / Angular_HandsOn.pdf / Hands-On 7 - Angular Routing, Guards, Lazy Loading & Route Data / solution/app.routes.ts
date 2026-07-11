9ba02b5640c8f8fb4df974f0f8c203672b31cbfb
Chalavadi Dnvh Subbarao <hanumach442@gmail.com>
2026-07-11 17:07:38 +0530

    Create app.routes.ts

---
+++ Angular / Angular_HandsOn.pdf / Hands-On 7 - Angular Routing, Guards, Lazy Loading & Route Data / solution/app.routes.ts
@@ -0,0 +1,54 @@
+import { Routes } from '@angular/router';
+
+import { HomeComponent } from './pages/home/home.component';
+import { CourseListComponent } from './pages/course-list/course-list.component';
+import { CourseDetailComponent } from './pages/course-detail/course-detail.component';
+import { StudentProfileComponent } from './pages/student-profile/student-profile.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { CoursesLayoutComponent } from './layouts/courses-layout/courses-layout.component';
+
+import { authGuard } from './guards/auth.guard';
+
+export const routes: Routes = [
+
+{
+path:'',
+component:HomeComponent
+},
+
+{
+path:'courses',
+component:CoursesLayoutComponent,
+children:[
+{
+path:'',
+component:CourseListComponent
+},
+{
+path:':id',
+component:CourseDetailComponent
+}
+]
+},
+
+{
+path:'profile',
+component:StudentProfileComponent,
+canActivate:[authGuard]
+},
+
+{
+path:'enroll',
+loadChildren:()=>import(
+'./features/enrollment/enrollment.module'
+).then(
+m=>m.EnrollmentModule
+)
+},
+
+{
+path:'**',
+component:NotFoundComponent
+}
+
+];
