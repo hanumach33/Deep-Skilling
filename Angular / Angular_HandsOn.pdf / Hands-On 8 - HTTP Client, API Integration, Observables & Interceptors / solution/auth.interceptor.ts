c20b0248eaa98c7a62df63464dbc93b647411ecd
Chalavadi Dnvh Subbarao <hanumach442@gmail.com>
2026-07-11 17:14:49 +0530

    Create auth.interceptor.ts

---
+++ Angular / Angular_HandsOn.pdf / Hands-On 8 - HTTP Client, API Integration, Observables & Interceptors / solution/auth.interceptor.ts
@@ -0,0 +1,21 @@
+import{
+HttpInterceptorFn
+}from '@angular/common/http';
+
+export const authInterceptor:HttpInterceptorFn=
+
+(req,next)=>{
+
+const clone=req.clone({
+
+setHeaders:{
+
+Authorization:'Bearer mock-token-12345'
+
+}
+
+});
+
+return next(clone);
+
+};
