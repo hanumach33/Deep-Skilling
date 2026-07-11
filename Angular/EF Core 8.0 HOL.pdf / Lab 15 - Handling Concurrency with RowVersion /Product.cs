24280aff43155eb39ec1efd68a21e4ceaabd330a
Chalavadi Dnvh Subbarao <hanumach442@gmail.com>
2026-07-11 13:19:18 +0530

    Create  Product.cs

---
+++ Angular/EF Core 8.0 HOL.pdf / Lab 15 - Handling Concurrency with RowVersion /Product.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace RetailInventory.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int StockQuantity { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+    }
+}
