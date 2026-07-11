cb5095e1beab5b1edf0a89fa1d61e92b40569857
Chalavadi Dnvh Subbarao <hanumach442@gmail.com>
2026-07-11 12:26:52 +0530

    Create  solution.sql

---
+++ Module 1 - Advanced SQL Server /Exception handling.pdf / Exercise 1 - Basic TRY...CATCH for Error Logging /solution.sql
@@ -0,0 +1,66 @@
+USE EmployeeManagementSystem;
+GO
+
+CREATE PROCEDURE AddEmployee
+    @EmployeeID INT,
+    @FirstName VARCHAR(50),
+    @LastName VARCHAR(50),
+    @Email VARCHAR(100),
+    @Salary DECIMAL(10,2),
+    @DepartmentID INT
+AS
+BEGIN
+    BEGIN TRY
+
+        INSERT INTO Employees
+        (
+            EmployeeID,
+            FirstName,
+            LastName,
+            Email,
+            Salary,
+            DepartmentID
+        )
+        VALUES
+        (
+            @EmployeeID,
+            @FirstName,
+            @LastName,
+            @Email,
+            @Salary,
+            @DepartmentID
+        );
+
+    END TRY
+
+    BEGIN CATCH
+
+        INSERT INTO AuditLog
+        (
+            Action,
+            ErrorMessage
+        )
+        VALUES
+        (
+            'Add Employee',
+            ERROR_MESSAGE()
+        );
+
+    END CATCH
+END;
+GO
+
+EXEC AddEmployee
+    1,
+    'John',
+    'Doe',
+    'john@example.com',
+    5000.00,
+    1;
+
+GO
+
+SELECT *
+FROM AuditLog;
+
+GO
