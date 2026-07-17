17458682e7074dd59608e9f5fd09742a9176bdf0
Chalavadi Dnvh Subbarao <hanumach442@gmail.com>
2026-07-17 19:23:29 +0530

    Create Employee.cs

---
+++ Web API / 4. WebApi_Handson.docx / Exercise 1 - Web API CRUD Operation / solution/Models/Employee.cs
@@ -0,0 +1,12 @@
+namespace EmployeeAPI.Models;
+
+public class Employee
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = "";
+
+    public int Salary { get; set; }
+
+    public bool Permanent { get; set; }
+}
