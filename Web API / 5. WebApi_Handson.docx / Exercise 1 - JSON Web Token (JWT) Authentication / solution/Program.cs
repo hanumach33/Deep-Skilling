450bb6addbf6a6286d650e3b6678e60d8f9f7088
Chalavadi Dnvh Subbarao <hanumach442@gmail.com>
2026-07-17 19:27:45 +0530

    Implement JWT authentication in Program.cs

---
+++ Web API / 5. WebApi_Handson.docx / Exercise 1 - JSON Web Token (JWT) Authentication / solution/Program.cs
@@ -0,0 +1,59 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+string securityKey = "mysuperdupersecret";
+
+var symmetricSecurityKey =
+    new SymmetricSecurityKey(
+        Encoding.UTF8.GetBytes(securityKey));
+
+builder.Services
+.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme =
+        JwtBearerDefaults.AuthenticationScheme;
+
+    options.DefaultChallengeScheme =
+        JwtBearerDefaults.AuthenticationScheme;
+
+    options.DefaultSignInScheme =
+        JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters =
+        new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+
+            ValidIssuer = "mySystem",
+            ValidAudience = "myUsers",
+            IssuerSigningKey = symmetricSecurityKey
+        };
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
