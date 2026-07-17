11f05ad7dd3bc8600a16d3b33115be21f5aa0aaf
Chalavadi Dnvh Subbarao <hanumach442@gmail.com>
2026-07-17 19:06:21 +0530

    Create EmployeeController.cs

---
+++ Web API / 2. WebApi_Handson.docx / Exercise 2 - Test Web API using POSTMAN / solution/Controllers/EmployeeController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class EmployeeController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetEmployees()
+    {
+        var employees = new List<object>
+        {
+            new
+            {
+                Id = 101,
+                Name = "John",
+                Department = "IT"
+            },
+            new
+            {
+                Id = 102,
+                Name = "David",
+                Department = "HR"
+            },
+            new
+            {
+                Id = 103,
+                Name = "Sara",
+                Department = "Finance"
+            }
+        };
+
+        return Ok(employees);
+    }
+}
