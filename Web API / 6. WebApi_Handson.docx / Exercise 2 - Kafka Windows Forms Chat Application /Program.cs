f5fec72f17810ac4ac39f87a3892bacb80c82b7c
Chalavadi Dnvh Subbarao <hanumach442@gmail.com>
2026-07-17 19:42:11 +0530

    Create Program.cs

---
+++ Web API / 6. WebApi_Handson.docx / Exercise 2 - Kafka Windows Forms Chat Application /Program.cs
@@ -0,0 +1,11 @@
+namespace ChatProducer;
+
+internal static class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        Application.Run(new Form1());
+    }
+}
