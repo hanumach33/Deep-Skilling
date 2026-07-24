f0c7459851f3e98a2981a3e6431d04f9ef0074b8
Chalavadi Dnvh Subbarao <hanumach442@gmail.com>
2026-07-24 19:35:32 +0530

    Create  UserGreeting.js

---
+++ React JS / 12. ReactJS-HOL.docx / Exercise 1 / solution / src /UserGreeting.js
@@ -0,0 +1,17 @@
+function UserGreeting() {
+
+    return (
+
+        <div>
+
+            <h2>Welcome Back</h2>
+
+            <h3>You can now book your flight tickets.</h3>
+
+        </div>
+
+    );
+
+}
+
+export default UserGreeting;
