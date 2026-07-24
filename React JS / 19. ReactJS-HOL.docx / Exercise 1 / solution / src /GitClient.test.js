630553fc36a6cec8dad9405aa6a35b82db70561c
Chalavadi Dnvh Subbarao <hanumach442@gmail.com>
2026-07-24 20:16:32 +0530

    Create  GitClient.test.js

---
+++ React JS / 19. ReactJS-HOL.docx / Exercise 1 / solution / src /GitClient.test.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import GitClient from "./GitClient";
+
+jest.mock("axios");
+
+describe("Git Client Tests", () => {
+
+    test("should return repository names for techiesyed", async () => {
+
+        const mockedData = {
+            data: [
+                { name: "RepoOne" },
+                { name: "RepoTwo" },
+                { name: "RepoThree" }
+            ]
+        };
+
+        axios.get.mockResolvedValue(mockedData);
+
+        const response =
+            await GitClient.getRepositories("techiesyed");
+
+        expect(axios.get).toHaveBeenCalledWith(
+            "https://api.github.com/users/techiesyed/repos"
+        );
+
+        expect(response.data).toEqual(mockedData.data);
+
+    });
+
+});
