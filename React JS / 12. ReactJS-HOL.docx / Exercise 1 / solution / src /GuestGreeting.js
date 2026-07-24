2c4ac753693b7b71ddf756175448cdc0345976df
Chalavadi Dnvh Subbarao <hanumach442@gmail.com>
2026-07-24 19:35:08 +0530

    Create  GuestGreeting.js

---
+++ React JS / 12. ReactJS-HOL.docx / Exercise 1 / solution / src /GuestGreeting.js
@@ -0,0 +1,21 @@
+function GuestGreeting() {
+
+    return (
+
+        <div>
+
+            <h2>Flight Details</h2>
+
+            <p>Flight : AI-202</p>
+            <p>From : Hyderabad</p>
+            <p>To : Delhi</p>
+
+            <h3>Please sign up.</h3>
+
+        </div>
+
+    );
+
+}
+
+export default GuestGreeting;
