30392f983893df7202355d317248ed359fec9106
Chalavadi Dnvh Subbarao <hanumach442@gmail.com>
2026-07-24 17:10:31 +0530

    Create  AssemblyInfo.cs

---
+++ NUnit and Moq / Parent project code.zip / UserManagerLib.zip / solution / Properties /AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UserManagerLib")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UserManagerLib")]
+[assembly: AssemblyCopyright("Copyright ©")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e5f2c3d1-0e12-4a74-9f8b-ab1234567890")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
