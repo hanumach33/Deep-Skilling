8170b0944e18f87027686e9b3e8c06536c65869a
Chalavadi Dnvh Subbarao <hanumach442@gmail.com>
2026-07-24 19:02:01 +0530

    Create  App.js

---
+++ React JS / 5. ReactJS-HOL.docx / Exercise 1 / solution / src /App.js
@@ -0,0 +1,41 @@
+import CohortDetails from "./CohortDetails";
+
+function App(){
+
+    return(
+
+        <div>
+
+            <h1>Cohorts Details</h1>
+
+            <CohortDetails
+                name="INTADMDF10 - .NET FSD"
+                startedOn="22-Feb-2022"
+                status="Scheduled"
+                coach="Ashmita"
+                trainer="Jojo Jose"
+            />
+
+            <CohortDetails
+                name="ADM21JF014 - Java FSD"
+                startedOn="10-Sep-2021"
+                status="Ongoing"
+                coach="Apoorv"
+                trainer="Elisa Smith"
+            />
+
+            <CohortDetails
+                name="CDBJF21025 - Java FSD"
+                startedOn="24-Dec-2021"
+                status="Ongoing"
+                coach="Ashmita"
+                trainer="John Doe"
+            />
+
+        </div>
+
+    );
+
+}
+
+export default App;
