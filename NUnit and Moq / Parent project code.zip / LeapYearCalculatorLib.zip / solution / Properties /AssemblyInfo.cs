e613e0a1cc50e797d02869982fa2bfff7dabcc4d
Chalavadi Dnvh Subbarao <hanumach442@gmail.com>
2026-07-24 17:08:31 +0530

    Create  AssemblyInfo.cs

---
+++ NUnit and Moq / Parent project code.zip / LeapYearCalculatorLib.zip / solution / Properties /AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LeapYearCalculatorLib")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LeapYearCalculatorLib")]
+[assembly: AssemblyCopyright("Copyright ©")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d2c9a58b-16b7-4a6a-be08-22c1e0a8b9e1")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
