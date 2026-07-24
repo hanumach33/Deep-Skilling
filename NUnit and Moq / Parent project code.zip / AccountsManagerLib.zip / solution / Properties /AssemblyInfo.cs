7ffb7a79cdccecc9ba66f5a0df7052ca6878c482
Chalavadi Dnvh Subbarao <hanumach442@gmail.com>
2026-07-24 17:00:41 +0530

    Create  AssemblyInfo.cs

---
+++ NUnit and Moq / Parent project code.zip / AccountsManagerLib.zip / solution / Properties /AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AccountsManagerLib")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AccountsManagerLib")]
+[assembly: AssemblyCopyright("Copyright © 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("69f6b8e8-3e32-4e32-ac3f-d92781775c30")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
