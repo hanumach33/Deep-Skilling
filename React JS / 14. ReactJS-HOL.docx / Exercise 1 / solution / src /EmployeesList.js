96f33b37271d5348c90c43af538a9d24eff09d4e
Chalavadi Dnvh Subbarao <hanumach442@gmail.com>
2026-07-24 19:49:51 +0530

    Create  EmployeesList.js

---
+++ React JS / 14. ReactJS-HOL.docx / Exercise 1 / solution / src /EmployeesList.js
@@ -0,0 +1,28 @@
+import EmployeeCard from "./EmployeeCard";
+
+function EmployeesList({ employees }) {
+
+    return (
+
+        <div>
+
+            {
+
+                employees.map(employee => (
+
+                    <EmployeeCard
+                        key={employee.id}
+                        employee={employee}
+                    />
+
+                ))
+
+            }
+
+        </div>
+
+    );
+
+}
+
+export default EmployeesList;
