5ddf76d7721350963b336d2c9050a431b6124076
Chalavadi Dnvh Subbarao <hanumach442@gmail.com>
2026-07-24 17:06:21 +0530

    Create  AssemblyInfo.cs

---
+++ NUnit and Moq / Parent project code.zip / FourSeasonsLib.zip / solution / Properties /AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FourSeasonsLib")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FourSeasonsLib")]
+[assembly: AssemblyCopyright("Copyright ©")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9b8d14b2-7e4c-4c2e-a0a3-1f4d0d4a8a91")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
