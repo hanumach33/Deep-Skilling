b918ff68addbb15bb0194c773493e4f362d4ec01
Chalavadi Dnvh Subbarao <hanumach442@gmail.com>
2026-07-24 19:28:40 +0530

    Create  App.js

---
+++ React JS / 11. ReactJS-HOL.docx / Exercise 1 / solution / src /App.js
@@ -0,0 +1,22 @@
+import EventExamples from "./EventExamples";
+import CurrencyConvertor from "./CurrencyConvertor";
+
+function App() {
+
+    return (
+
+        <div>
+
+            <EventExamples />
+
+            <hr />
+
+            <CurrencyConvertor />
+
+        </div>
+
+    );
+
+}
+
+export default App;
